de0db261d08034078ab988064c808bfa5d3854e3
MOYU114 <moyu114@outlook.com>
2024-03-25 21:03:53 +0800

    将恶意检测整合为类，并加入计时更新机制（应该能用）

--- malicious_detection.py
+++ malicious_detection.py
@@ -10,75 +10,129 @@ import utils
 import utils_model
 import matplotlib.pyplot as plt
 import time
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-# param
-ev_input_dim = 28
-ev_latent_dim = 64
-es_input_dim = 10
-es_hidden_dim = 300
-dv_output_dim = 28
-threshold = 400
-lamb=11
-# path
-CSI_PATH = "./data/static/CSI_static_6C.csv"
-Video_PATH = "./data/static/points_static.csv"
-MODEL_OUTPUT_PATH = "./model/"
-CSI_AVG_PATH ="./data/static/CSI_static_6C_avg.csv"
-LOG_PATH="./output/log.txt"
-UPDATE = True
-Alarm = False
-# 假定这是经过了固定的时间T收集的到的CSI数据
-with open(CSI_PATH, "r", encoding='utf-8-sig') as csvfile:
-    csvreader = csv.reader(csvfile)
-    data1 = list(csvreader)
-aa = pd.DataFrame(data1)  # 读取CSI数据到aa
+import threading
 
-#需要一个时间函数，去判断是否到了更新数据的阶段
-if UPDATE:
-    # calculate CSI_avg
-    print("update threshold.")
-    '''
-    if (os.path.exists(CSI_AVG_PATH) != True):
-        CSI_avg = utils.cal_avg(aa)
-        np.savetxt(CSI_AVG_PATH, CSI_avg, delimiter=',')
-    else:
-        with open(CSI_AVG_PATH, "r", encoding='utf-8-sig') as csvfile:
+
+
+class detection_proc:
+
+    device = torch.device('cpu')
+    # param
+    ev_input_dim = 28
+    ev_latent_dim = 64
+    es_input_dim = 10
+    es_hidden_dim = 300
+    dv_output_dim = 28
+    threshold = 400
+    lamb = 11
+    minute=5
+    # path
+    HISTORY_CSI_LOG_PATH = "./data/static/CSI_static_6C.csv"
+    CRR_CSI_PATH = "./data/static/CSI_new.csv"
+    MODEL_OUTPUT_PATH = "./model/"
+    CSI_AVG_PATH = "./data/static/CSI_static_6C_avg.csv"
+    LOG_PATH = "./output/log.txt"
+    # sign
+    UPDATE=True
+    threadLock = threading.Lock()
+    def __init__(self):
+        os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    def __readCSI(self,csi_path):
+        with open(csi_path, "r", encoding='utf-8-sig') as csvfile:
             csvreader = csv.reader(csvfile)
-            data1 = list(csvreader)  # 将读取的数据转换为列表
-        CSI_avg = pd.DataFrame(data1)
-    #CSI_avg = utils.reshape_and_average(aa)  # 把多个CSI数据包平均为一个数据包，使一帧对应一个CSI数据包
-    '''
-    CSI_avg = utils.cal_avg(aa)
-    CSI_avg = CSI_avg.T.squeeze()
-    CSI_avg = CSI_avg.values.astype('float32')
-    print("get CSI_avg")
-    CSI_avg_median = np.median(CSI_avg)
-    CSI_avg_mad = np.median(np.abs(CSI_avg - np.median(CSI_avg)))
-    threshold= lamb*CSI_avg_mad+CSI_avg_median
+            data1 = list(csvreader)
+        aa = pd.DataFrame(data1)  # 读取CSI数据到aa
+        return aa
 
-# 假设这是一秒内实时接收到的数据，假设15帧/s
+    def updateThreshold(self,HISTORY_CSI_LOG_PATH):
+        '需要一个时间函数，去判断是否到了更新数据的阶段'
+        # calculate CSI_avg
+        print("update threshold.")
+        '''
+        if (os.path.exists(CSI_AVG_PATH) != True):
+            CSI_avg = utils.cal_avg(aa)
+            np.savetxt(CSI_AVG_PATH, CSI_avg, delimiter=',')
+        else:
+            with open(CSI_AVG_PATH, "r", encoding='utf-8-sig') as csvfile:
+                csvreader = csv.reader(csvfile)
+                data1 = list(csvreader)  # 将读取的数据转换为列表
+            CSI_avg = pd.DataFrame(data1)
+        #CSI_avg = utils.reshape_and_average(aa)  # 把多个CSI数据包平均为一个数据包，使一帧对应一个CSI数据包
+        '''
+        aa = self.__readCSI(HISTORY_CSI_LOG_PATH)
+        CSI_avg = utils.cal_avg(aa)
+        CSI_avg = CSI_avg.T.squeeze()
+        CSI_avg = CSI_avg.values.astype('float32')
+        print("get CSI_avg")
+        CSI_avg_median = np.median(CSI_avg)
+        CSI_avg_mad = np.median(np.abs(CSI_avg - np.median(CSI_avg)))
+        self.threshold = self.lamb * CSI_avg_mad + CSI_avg_median
+    def updateHistoryLog(self,CRR_CSI_PATH,HISTORY_CSI_LOG_PATH):
+        aa = self.__readCSI(HISTORY_CSI_LOG_PATH)
+        bb = self.__readCSI(CRR_CSI_PATH)
+        aa=pd.concat([aa,bb], ignore_index = True)
+        # 之后考虑5min内收集多少行数据，超过该数据则要对history数据清理
+        aa.to_csv(HISTORY_CSI_LOG_PATH, header=None, index=None)
 
-crr_CSI= utils.cal_single_avg(aa[:15])
-Alarm=True
-#Alarm=crr_CSI>threshold
+    def isAlarm(self,NEW_CSI_PATH):
+        crr_aa = self.__readCSI(NEW_CSI_PATH)
+        crr_CSI = utils.cal_single_avg(crr_aa)
+        flag = crr_CSI > self.threshold
+        return crr_CSI, flag
+
+    def alarm(self,crr_CSI):
+        begin_t = int(round(time.time() * 1000))
+        begin = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(begin_t / 1000))
+        # while(crr_CSI>threshold):
+        #    now = int(round(time.time() * 1000))
+        time.sleep(1)
+        now = int(round(time.time() * 1000))
+        end = time.strftime('%H:%M:%S', time.localtime(now / 1000))
+
+        logger = utils.logger_config(log_path=self.LOG_PATH, logging_name='test')
+        logger.warning(begin + "~" + end + " 检测到有人经过。")
+    def __m2s(self,minute):
+        return minute*60
+    def __canUpdate(self,min):
+        while(1):
+            print("running update")
+            #begin_time = time.time()
+            time.sleep(self.__m2s(min))
+            #crr_time = time.time()
+            #print(int(crr_time-begin_time))
+            #lock
+            self.threadLock.acquire()
+            self.UPDATE = True
+            self.threadLock.release()
+
+    def run(self):
+        #单开一个线程计时
+        t1 = threading.Thread(target=self.__canUpdate, args=(self.minute,))
+        t1.start()
+        while(1):
+            if(self.UPDATE):#每五分钟更新一次
+                self.updateThreshold(self.HISTORY_CSI_LOG_PATH)
+                #lock
+                self.threadLock.acquire()
+                self.UPDATE = False
+                self.threadLock.release()
+
+            if(False):#有新数据传入
+                crr_CSI,flag=self.isAlarm(self.CRR_CSI_PATH)
+                if(flag):
+                    self.alarm(crr_CSI)
+                else:
+                    #将当前数据传入历史记录中
+                    self.updateHistoryLog(self.CRR_CSI_PATH,self.HISTORY_CSI_LOG_PATH)
+            now = int(round(time.time() * 1000))
+            end = time.strftime('%H:%M:%S', time.localtime(now / 1000))
+            print(end)
 
 
-
-
-if(Alarm):
-    begin_t = int(round(time.time() * 1000))
-    begin = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(begin_t / 1000))
-    #while(crr_CSI>threshold):
-    #    now = int(round(time.time() * 1000))
-    time.sleep(1)
-    now = int(round(time.time() * 1000))
-    end = time.strftime('%H:%M:%S', time.localtime(now / 1000))
-    Alarm = False
-    logger = utils.logger_config(log_path=LOG_PATH, logging_name='test')
-    logger.warning(begin+"~"+end+" 检测到有人经过。")
-
-tt=CSI_avg.squeeze()
-plt.plot(np.arange(0,4800),np.full(4800,threshold))
-plt.plot(np.arange(0,4800),tt[:4800])
-plt.show()
+if __name__ == '__main__':
+    #test1="./data/test/csi1.csv"
+    #test2="./data/test/csi2.csv"
+    test=detection_proc()
+    test.run()
